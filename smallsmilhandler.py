9ec643bd5126d3835a8faab817fe252c3204ce8d
Pablo <elrodas95@gmail.com>
2016-10-09 19:33:43 +0200

    diccionario creado

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -2,3 +2,37 @@
 # -*- coding: utf-8 -*-
 
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+
+class SmallSMILHandler(ContentHandler):
+	
+	def __init__(self):
+		self.dicc = {}
+		self.etiquetas = {"root-layout","region","img","audio","textstream"}
+
+		self.atributos = {
+			"root-layout": ["width", "height", "background-color"],
+			"region": ["id", "top", "bottom", "left", "right"],
+			"img": ["scr", "region", "begin", "dur"],
+			"audio": ["scr", "begin", "dur"],
+			"textstream": ["scr", "region"]
+			}
+	
+	def startElement(self, name, attrs):
+		if name in self.etiquetas:
+			
+			
+			
+
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil.xml'))
+
+

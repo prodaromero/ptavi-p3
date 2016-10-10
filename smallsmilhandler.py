2753ef506e8e01fb48fffc762c79d48996f5b4c2
Pablo Roda Romero <proda@gamma39.aulas.gsyc.urjc.es>
2016-10-10 10:42:42 +0200

    Apartado 3 terminado

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,39 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+class SmallSMILHandler(ContentHandler):
+
+    def __init__(self):
+
+        self.atributos = []
+        self.etiquetas = {
+            "root-layout": ["width", "height", "background-color"],
+            "region": ["id", "top", "bottom", "left", "right"],
+            "img": ["scr", "region", "begin", "dur"],
+            "audio": ["scr", "begin", "dur"],
+            "textstream": ["scr", "region"]
+            }
+
+    def startElement(self, name, attrs):
+        if name in self.etiquetas:
+            dicc = {} 
+            for etiqueta in self.etiquetas[name]:
+                dicc[etiqueta] = attrs.get(etiqueta, "")
+            self.atributos.append([name, dicc])
+            
+    def get_tags(self):
+        return self.atributos
+
+
+if __name__ == "__main__":
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil.xml'))
+    
+    Dicc = cHandler.get_tags()
+    print(Dicc)
 
